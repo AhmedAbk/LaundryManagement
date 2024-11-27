ace77cfa39a387fc762e2a7886152d68cec4c554
Ahmed Abdelkader <122270710+AhmedAbk@users.noreply.github.com>
2024-11-27 14:55:49 +0100

    deleting some unused fils and some comments

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -6,22 +6,19 @@ using Business.Services;
 using Infrastructure.DAO;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container
+ 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Laundry API", Version = "v1" });
 });
-
-// Register services with direct connection string
+ 
 string connectionString = "server=localhost;user=root;database=laundry;port=3306;password=";
 builder.Services.AddScoped<ILaundryService>(provider => new LaundryService(connectionString));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline
+ 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
